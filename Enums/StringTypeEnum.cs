b5608e687144fb5c01e517b7778ca210a046dd5d
jmlacal <jm.lacal@outlook.com.ar>
2023-09-04 14:32:35 -0300

    limpieza de codigo

--- Enums/StringTypeEnum.cs
+++ Enums/StringTypeEnum.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestApp.Enums
+﻿namespace TestApp.Enums
 {
     public enum StringTypeEnum
     {
